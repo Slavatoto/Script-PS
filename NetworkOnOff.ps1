677e515bfdf1ac73c39280363c83fd53ab6a134b
slavatoto <antoine.morillon@protonmail.com>
2025-02-15 11:35:36 +0100

    Final Commit

--- NetworkOnOff.ps1
+++ NetworkOnOff.ps1
@@ -1,14 +1,13 @@
-# Spécifiez le nom de la carte réseau que vous souhaitez gérer
 $nomCarteReseau = "Ethernet1"
 
-# Obtenez l'objet de la carte réseau en utilisant le nom
+# Retourne l'objet de la carte réseau en utilisant le nom
 $carteReseau = Get-WmiObject Win32_NetworkAdapter | Where-Object {$_.NetConnectionID -eq $nomCarteReseau}
 
-# Vérifiez si la carte réseau a été trouvée
+# Vérifie si la carte réseau a été trouvée
 if ($carteReseau -eq $null) {
     Write-Host "La carte réseau '$nomCarteReseau' n'a pas été trouvée."
 } else {
-    # Vérifiez si la carte réseau est actuellement activée ou désactivée
+    # Vérifie si la carte réseau est actuellement activée ou désactivée
     if ($carteReseau.NetConnectionStatus -eq 2) {
         Write-Host "Désactivation de la carte réseau '$nomCarteReseau'..."
         $carteReseau.Disable()
@@ -19,6 +18,4 @@ if ($carteReseau -eq $null) {
         Write-Host "La carte réseau '$nomCarteReseau' a été activée."
     }
 }
-
-#ceci est un test
  
